736a6a64e7da2b2ed537ea32847381c9fc625874
tenshokushitaiman <knock.822a869@gmail.com>
2023-02-12 13:05:39 +0000

    セレクターの指定方法について学習した。

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { Routes, Route } from "react-router-dom"
 import '@views/home/Home.css'
 import App from '@views/app/App'
+import {TagSelector, IdSelector, CombinationSelector} from '@views/cssSelector/tagSelector/TagSelector'
 
 function Home() {
   const [count, setCount] = useState(0)
@@ -12,17 +13,19 @@ function Home() {
         <nav>
           <ul className='nav__items'>
           <li className='nav__item'><a href="/">home</a></li>
-            <li className='nav__item'><a href="/app">app</a></li>
+          <li className='nav__item'><a href="/app">app</a></li>
+          <li className='nav__item'><a href="/chapter2/tagSelector">chapter2_tagSelector</a></li>
+          <li className='nav__item'><a href="/chapter2/idSelector">chapter2_idSelector</a></li>
+          <li className='nav__item'><a href="/chapter2/combinationSelector">chapter2_combinationSelector</a></li>
+
           </ul>
         </nav>
       </header>
-      <div className='container'>
-        <button>
-          hello
-        </button>
-      </div>
       <Routes>
         <Route path='/app' element={<App />}></Route>
+        <Route path='/chapter2/tagSelector' element={<TagSelector/>}></Route>
+        <Route path='/chapter2/idSelector' element={<IdSelector/>}></Route>
+        <Route path='/chapter2/combinationSelector' element={<CombinationSelector/>}></Route>
       </Routes>
     </div>
   )
