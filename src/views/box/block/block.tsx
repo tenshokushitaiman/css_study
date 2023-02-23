a72bdc1b835766cbecf0f4edc9ce96150a8bfacd
tenshokushitaiman <knock.822a869@gmail.com>
2023-02-23 02:27:28 +0000

    box: displayの値 block, inline, inline-blockの違いについてまとめた

--- src/views/box/block/block.tsx
+++ src/views/box/block/block.tsx
@@ -1,12 +1,37 @@
-import "./block.css"
+import style from "./block.module.css"
 function BoxBlock() {
-    return (
-      <div>
-          <h1>this is a heading</h1>
-          <p>this is a paragraph.</p>
+  return (
+    <div>
+      <h1>Display property example</h1>
+
+      <h2>Block example:</h2>
+      <div className={style["block-container"]}>
+        <div className={style["block-element"]}>
+          <p>This is a block element. It takes up the entire width of its parent container and starts on a new line.</p>
+        </div>
+        <div className={style["block-element"]}>
+          <p>This is another block element. It also takes up the entire width of its parent container and starts on a new line.</p>
+        </div>
       </div>
-    )
-  }
+
+      <h2>Inline-block example:</h2>
+      <div className={style["inline-block-container"]}>
+        <div className={style["inline-block-element"]}>
+          <p>This is an inline-block element. It only takes up as much width as it needs, but can have a specified height.</p>
+        </div>
+        <div className={style["inline-block-element"]}>
+          <p>This is another inline-block element. It only takes up as much width as it needs, but can have a specified height.</p>
+        </div>
+      </div>
+
+      <h2>Inline example:</h2>
+      <div className={style["inline-container"]}>
+        <span className={style["inline-element"]}>This is an inline element. It only takes up as much width as it needs, and does not start on a new line.</span>
+        <span className={style["inline-element"]}>This is another inline element. It only takes up as much width as it needs, and does not start on a new line.</span>
+      </div>
+    </div>
+  )
+}
 export {
-BoxBlock
+  BoxBlock
 }
